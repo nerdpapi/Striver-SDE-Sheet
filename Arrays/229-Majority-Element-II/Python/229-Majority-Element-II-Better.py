9ca975cbe8186b61759784fc67f959fd16c3dd7d
nerdpapi <kunalkmr71@gmail.com>
2026-09-18 19:00:09 +0530

    Add Striver SDE Majority Element II

--- Arrays/229-Majority-Element-II/Python/229-Majority-Element-II-Better.py
+++ Arrays/229-Majority-Element-II/Python/229-Majority-Element-II-Better.py
@@ -1,24 +1,28 @@
 """
-Problem: 169. Majority Element
+Problem: 229. Majority Element II
 Approach: Better - Hash Map
 
 Store the frequency of each element.
-As soon as an element's frequency becomes greater than n / 2,
-return it.
+Whenever an element's frequency becomes greater than n / 3,
+add it to the result.
 
 Time Complexity: O(n)
 Space Complexity: O(n)
 """
 
 class Solution:
-    def majorityElement(self, nums: list[int]) -> int:
+    def majorityElement(self, nums: list[int]) -> list[int]:
         n = len(nums)
         freq = {}
+        result = []
 
         for num in nums:
             freq[num] = freq.get(num, 0) + 1
 
-            if freq[num] > n / 2:
-                return num
+            if freq[num] > n / 3 and num not in result:
+                result.append(num)
 
-        return -1
+            if len(result) == 2:
+                break
+
+        return result

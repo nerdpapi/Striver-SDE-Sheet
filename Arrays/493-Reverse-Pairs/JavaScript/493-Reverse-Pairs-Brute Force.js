7071bef503f5e012ab600ac2602ae0d9fabde491
nerdpapi <kunalkmr71@gmail.com>
2026-09-19 19:11:00 +0530

    Add Striver SDE Reverse Pairs

--- Arrays/493-Reverse-Pairs/JavaScript/493-Reverse-Pairs-Brute Force.js
+++ Arrays/493-Reverse-Pairs/JavaScript/493-Reverse-Pairs-Brute Force.js
@@ -1,47 +1,30 @@
 /**
- * Problem: 75. Sort Colors
- * Approach: Better - Counting
+ * Problem: 493. Reverse Pairs
+ * Approach: Brute Force
  *
- * Count the number of 0s, 1s, and 2s, then overwrite the array
- * with the required number of each value.
+ * Check every pair (i, j) where i < j.
+ * A reverse pair exists when:
+ *     nums[i] > 2 * nums[j]
  *
- * Time Complexity: O(n)
+ * Time Complexity: O(n^2)
  * Space Complexity: O(1)
  */
 
 /**
  * @param {number[]} nums
- * @return {void}
+ * @return {number}
  */
+var reversePairs = function(nums) {
+    const n = nums.length;
+    let count = 0;
 
-var sortColors = function(nums) {
-    let count0 = 0;
-    let count1 = 0;
-    let count2 = 0;
-
-    // Count 0, 1 and 2
-    for (const num of nums) {
-        if (num === 0) {
-            count0++;
-        } else if (num === 1) {
-            count1++;
-        } else {
-            count2++;
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if (nums[i] > 2 * nums[j]) {
+                count++;
+            }
         }
     }
 
-    // Fill 0s
-    for (let i = 0; i < count0; i++) {
-        nums[i] = 0;
-    }
-
-    // Fill 1s
-    for (let i = count0; i < count0 + count1; i++) {
-        nums[i] = 1;
-    }
-
-    // Fill 2s
-    for (let i = count0 + count1; i < nums.length; i++) {
-        nums[i] = 2;
-    }
+    return count;
 };
